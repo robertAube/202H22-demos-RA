2a497c7bea2eea39d8a6dd50ff0d1fac1cf8727a
Robert Aubé <robert.aube@cegeplimoilou.ca>
2022-02-02 12:04:17 -0500

    Mise à jour de DemoException

--- crs3Exception/src/main/java/exceptions/DemoExceptions.java
+++ crs3Exception/src/main/java/exceptions/DemoExceptions.java
@@ -6,8 +6,8 @@ import Utilitaire.Util;
  * Diagramme partiel de la hiérarchie des Exception en Java:
  * Object
  * ├── Throwable
- * │   └── Exception
- * │       ├── RuntimeException (N'ont pas besoin d'être dans des blocs try/catch)
+ *    └── Exception
+ * │##     ├── RuntimeException (N'ont pas besoin d'être dans des blocs try/catch)
  * │       │   ├── ArithmeticException
  * │       │   ├── IllegalArgumentException
  * │       │   │   └── NumberFormatException
@@ -17,19 +17,50 @@ import Utilitaire.Util;
  * │       └── IOException
  * │           └── FileNotFoundException
  * └── Error
- *     Les exceptions héritant de la classe RuntimeException n'ont pas besoin d'être détectées impérativement par des blocs try/catch.
+ * Les exceptions héritant de la classe RuntimeException n'ont pas besoin d'être détectées impérativement par des blocs try/catch.
  */
 
 public class DemoExceptions {
+
     public DemoExceptions() {
-        Util.lireInt("Test");
+     //   System.out.println(lireEntierAvecCatch("Donner un nombre entier : "));
+
         demoException1();
     }
 
-    private void demoException1() {
+    private int lireEntierAvecCatch(String s) {
+        boolean estInvalide = true;
+        int valeurLu = 0;
+        do {
+            try {
+                valeurLu = Util.lireInt(s);
+                estInvalide = false;
+            } catch (NumberFormatException e) {
+                System.out.println("Format de nombre incorrect : veuillez recomencer!");
+            }
+        }
+        while (estInvalide);
+        return valeurLu;
     }
 
-    private void lanceUneException()  {
+    private void demoException1() {
+        try {
+            lanceUneException(new Exception("Exception 1"));
+        }
+        catch (Throwable e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void lanceUneException(Throwable exceptionALancer) throws Throwable {
+//        try {
+//        throw exceptionALancer;}
+//        catch (Throwable e) {
+//
+//        }
+
+        throw exceptionALancer;
     }
 
     public static void main(String[] args) {
