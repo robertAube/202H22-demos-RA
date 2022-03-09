4e9697a914baaa40ebf75d47242af1ad465afa1a
Robert Aubé <robert.aube@cegeplimoilou.ca>
2022-03-08 23:28:42 -0500

    MAJ de IJeuMemoire.java pour clarification des x et y par colonne et ligne.

--- 202H21 - Tp3 - Étudiant/IJeuMemoireOld.java
+++ 202H21 - Tp3 - Étudiant/IJeuMemoireOld.java
@@ -9,16 +9,16 @@ public interface IJeuMemoire
 	public static final int NIVEAU_MAX = 6;
 
 	/**
-	 * L'intelligence du jeu. Génère un tableau de coordonnées (des objets <b>Point(colonne, ligne)</b>
-	 * au hasard. Les points générés doivent être valides dans la grille de jeu.
-	 * Le nombre de points générés est en relation avec le niveau courant du jeu.
-	 * Il obéit à la règle suivante:
-	 *
+	 * L'intelligence du jeu. Génère un tableau de coordonnées (des objets Point
+	 * (x et y)) au hasard. Les points générés doivent être valides dans la
+	 * grille de jeu. Le nombre de points générés est en relation avec le niveau
+	 * courant du jeu. Il obéit à la règle suivante:
+	 * 
 	 * Le nombre de points générés = niveau courant du jeu + 2
-	 *
+	 * 
 	 * <b>Il est important qu'un même point ne soit pas choisi 2 fois.</b>
-	 *
-	 * @return la liste des coordonnées <b>Point(colonne, ligne)</b> des formes choisies dans la grille.
+	 * 
+	 * @return la liste des coordonnées des formes choisies dans la grille.
 	 */
 	public ArrayList<Point> jouerOrdi();
 
@@ -26,27 +26,27 @@ public interface IJeuMemoire
 	 * Valide si la coordonnée jouée par le joueur humain est valide et dans
 	 * l'ordre selon les coordonnées générées par l'ordinateur. Voir la méthode
 	 * "jouerOrdi()"
-	 *
-	 * @param ligne coordonnée ligne de la grille
-	 * @param colonne coordonnée colonne dans la grille
+	 * 
+	 * @param x coordonnée x dans la grille (x = colonne)
+	 * @param y coordonnée x dans la grille (y = ligne)
 	 * @return oui ou non si la coordonnée du joueur est la coordonnée jouée par
 	 *         l'ordi dans l'ordre respecté
 	 */
-	public boolean jouerHumain(int ligne, int colonne);
+	public boolean jouerHumain(int x, int y);
 
 	/**
 	 * Obtenir sous forme de chaîne sans espace le nom et la couleur de la forme
 	 * à l'emplacement désiré dans la grille.
-	 *
-	 * @param ligne coordonnée ligne de la matrice grille
-	 * @param colonne coordonnée colonne de la matrice grille
+	 * 
+	 * @param x coordonnée x de la matrice grille (x = colonne)
+	 * @param y coordonnée y de la matrice grille (y = ligne)
 	 * @return nom et couleur concaténée en chaîne de caractère
 	 */
-	public String getNomForme(int ligne, int colonne);
+	public String getNomForme(int x, int y);
 
 	/**
 	 * Connaître le niveau du jeu.
-	 *
+	 * 
 	 * @return le niveau du jeu
 	 */
 	public int getNiveau();
