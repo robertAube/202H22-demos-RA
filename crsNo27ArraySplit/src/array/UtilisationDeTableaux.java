b71562469f9ccf8384f38921a174127bf52363e9
Robert Aubé <robert.aube@cegeplimoilou.ca>
2022-05-09 16:07:56 -0400

    Cours 27 - split() & Arrays - MAJ

--- crsNo27ArraySplit/src/array/UtilisationDeTableaux.java
+++ crsNo27ArraySplit/src/array/UtilisationDeTableaux.java
@@ -3,49 +3,70 @@ package array;
 import java.util.Arrays;
 
 public class UtilisationDeTableaux {
-    private int valeursInt[] = {10, 2, 35, 40, 55, 6};
-    private int rempliDInt[];
-    private int copieValeursInt[] = {10, 2, 35, 40, 55, 6};
-
-    private void afficherTableau(int[] tab) {
-        String str = "";
-        for (int element : tab) //for each…{
-            str += element + ", ";
-        System.out.print("[" + (tab.length == 0 ? "" : str.substring(0, str.length() - 2)) + "]");
-    }
 
     public UtilisationDeTableaux() {
         remplirTableau();
-
-
-
-        System.out.print("\n\nvaleursInt : ");
-        System.out.println(Arrays.toString(rempliDInt));
-        Arrays.sort(valeursInt); // trier valeurs
-        System.out.print("\nArrays.sort(valeursInt) : ");
-        System.out.println(Arrays.toString(rempliDInt));
-
-        // Trouver la valeur dans le tableau valeursInt.
-        System.out.println("\n\nLa position de 55 est " +
-                Arrays.binarySearch(valeursInt, 55));
-        System.out.println("La position de 355 est " +
-                Arrays.binarySearch(valeursInt, 355));
-
-        //Vérifier l'égalité
-        System.out.print("\nArrays.equals(valeursInt, copieValeursInt) = ");
-        System.out.println(Arrays.equals(valeursInt, copieValeursInt));
-        System.out.print("Arrays.equals(valeursInt, rempliDInt) = ");
-        System.out.println(Arrays.equals(valeursInt, rempliDInt));
+        trierTableau();
+        trouverValeurTableau();
+        verifierEgalite();
     }
 
     private void remplirTableau() {
-        rempliDInt = new int[10];
+        int valeursInt[] = new int[10];
 
-        System.out.println("remplirTableau()");
-        System.out.println("Tableau avant Arrays.fill" + Arrays.toString(rempliDInt));
-        Arrays.fill(rempliDInt, 7); // remplir avec des 7
-        System.out.print("\nArrays.fill(rempliDInt, 7) : ");
-        System.out.println(Arrays.toString(rempliDInt));
+        System.out.println("\n## remplirTableau() ###########");
+        System.out.println("Tableau avant Arrays.fill : " + Arrays.toString(valeursInt));
+        Arrays.fill(valeursInt, 7); // remplir avec des 7
+        System.out.println("Arrays.fill(rempliDInt, 7) :" + Arrays.toString(valeursInt));
+    }
+
+    private void trierTableau() {
+        int valeursInt[] = {10, 2, 35, 40, 55, 6};
+
+        System.out.println("\n## trierTableau() ###########");
+        System.out.print("valeursInt : ");
+        System.out.println(Arrays.toString(valeursInt));
+        Arrays.sort(valeursInt); // trier valeurs
+        System.out.print("Arrays.sort(valeursInt) : ");
+        System.out.println(Arrays.toString(valeursInt));
+    }
+
+    private void trouverValeurTableau() {
+        int valeursInt[] = {10, 2, 35, 40, 55, 6, 1};
+
+        System.out.println("\n## trouverValeurTableau() ###########");
+        System.out.println(Arrays.toString(valeursInt));
+        System.out.println("La position de 55 est " +
+                //retourne l'index de l'élément recherché ou une valeur négative si ne trouve pas
+                Arrays.binarySearch(valeursInt, 55));
+        System.out.println("La position de 355 est " +
+                Arrays.binarySearch(valeursInt, 355));
+        Arrays.sort(valeursInt); // trier valeurs
+        System.out.println(Arrays.toString(valeursInt));
+        System.out.println("La position de 55 est " +
+                Arrays.binarySearch(valeursInt, 55));
+        System.out.println("La position de 355 est " +
+                Arrays.binarySearch(valeursInt, 355));
+    }
+
+    //Vérifier l'égalité et copy
+    private void verifierEgalite() {
+        int valeursInt[] = {10, 2, 35, 40, 55, 6};
+        //Copie les éléments dans un nouveau tableau. (Notez qu'on réfère au même objet si c'est un tableau d'objets)
+        int valeursIntCopy[] = Arrays.copyOf(valeursInt, valeursInt.length);
+
+        System.out.println("\n## verifierEgalite() ###########");
+
+        System.out.println("valeursInt[] = " + Arrays.toString(valeursInt));
+        System.out.println("valeursIntClone[] = " + Arrays.toString(valeursIntCopy));
+        System.out.print("Arrays.equals(valeursInt, valeursIntClone) = ");
+        System.out.println(Arrays.equals(valeursInt, valeursIntCopy));
+
+        valeursIntCopy[2] = 13;
+        System.out.println("valeursInt[] = " + Arrays.toString(valeursInt));
+        System.out.println("valeursIntClone[] = " + Arrays.toString(valeursIntCopy));
+        System.out.print("Arrays.equals(valeursInt, valeursIntClone) = ");
+        System.out.println(Arrays.equals(valeursInt, valeursIntCopy));
     }
 
     public static void main(String[] args) {
